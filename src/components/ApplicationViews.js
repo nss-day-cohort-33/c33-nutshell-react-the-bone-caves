7cd89558e38a5b8d7ae708f4008ebc35d7f384bb
Tyler Carpenter <tcarp55@gmail.com>
2019-07-22 15:39:59 -0500

    got the will mount thing working

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,10 +2,37 @@ import { Route, Redirect } from "react-router-dom";
 import React, { Component } from "react";
 import Login from "./login/Login"
 import Register from './register/register'
+import UserHandler from "./apiManager/UserHandler"
+import ArticleHandler from "./apiManager/ArticleHandler"
+import EventHandler from "./apiManager/EventHandler"
+import TaskHandler from "./apiManager/TaskHandler"
+import MessageHandler from "./apiManager/MessageHandler"
 
 export default class ApplicationViews extends Component {
+  state = {
+    users: [],
+    articles: [],
+    events: [],
+    tasks: [],
+    messages: []
+  };
+
+  componentDidMount() {
+    UserHandler
+      .getAll()
+      .then(users => this.setState({ users: users }))
+      .then(() => ArticleHandler.getAll())
+      .then(articles => this.setState({ articles: articles }))
+      .then(() => EventHandler.getAll())
+      .then(events => this.setState({ events: events }))
+      .then(() => TaskHandler.getAll())
+      .then(tasks => this.setState({ tasks: tasks }))
+      .then(() => MessageHandler.getAll())
+      .then(messages => this.setState({ messages: messages }));
+  }
 
   render() {
+    console.log(this.state)
     return (
       <React.Fragment>
 
@@ -20,7 +47,7 @@ export default class ApplicationViews extends Component {
             return <Register />
           }}
         />
-        
+
         <Route
           path="/friends" render={props => {
             // Remove null and return the component which will show list of friends
