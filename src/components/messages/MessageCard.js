0624a0c6476883afc000b1b41b61d375f6cd36b6
Tyler Carpenter <tcarp55@gmail.com>
2019-07-24 14:26:22 -0500

    got something to work with inline edit

--- src/components/messages/MessageCard.js
+++ src/components/messages/MessageCard.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
+import EditableLabel from 'react-inline-editing';
 
 export default class MessageCard extends Component {
+  constructor(props){
+    super(props);
+
+    this._handleFocus = this._handleFocus.bind(this);
+    this._handleFocusOut = this._handleFocusOut.bind(this);
+  }
+  
+  _handleFocus(text) {
+    console.log('Focused with text: ' + text);
+}
+
+_handleFocusOut(text) {
+    console.log('Left editor with text: ' + text);
+}
   render() {
     return (
       <div key={this.props.message.id} className="card">
         <div className="card-body">
           <div className="card-title">
             <h5>{this.props.message.username}</h5>
-            <p>{this.props.message.message}</p>
+            <EditableLabel text={this.props.message.message} onFocus={this._handleFocus}
+                onFocusOut={this._handleFocusOut}>{this.props.message.message}</EditableLabel>
             <a href="#"
               onClick={() => this.props.deleteMessage(this.props.message.id)}
               className="card-link"
