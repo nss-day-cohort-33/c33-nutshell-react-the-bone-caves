079285b0ec6fcc4771fe662e5c579ea27a3a7fff
Tyler Carpenter <tcarp55@gmail.com>
2019-07-24 15:49:58 -0500

    things

--- src/components/messages/MessageCard.js
+++ src/components/messages/MessageCard.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import EditableLabel from 'react-inline-editing';
+import EditableLabel from "react-inline-editing";
 
 export default class MessageCard extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this._handleFocus = this._handleFocus.bind(this);
@@ -10,29 +10,43 @@ export default class MessageCard extends Component {
   }
 
   _handleFocus(text) {
-    console.log('Focused with text: ' + text);
-}
-
-_handleFocusOut(text) {
-  let newMessage = {
-    userId: +sessionStorage.getItem("userId"),
-    username: this.props.message.username,
-    message: text,
-    timestamp: this.props.message.timestamp,
-    id: this.props.message.id
-
+    console.log("Focused with text: " + text);
   }
-    console.log(newMessage);
-}
+
+  keyPress(e){
+    if(e.keyCode == 13){
+       console.log('value', e.target.text);
+       // put the login here
+    }
+ };
+
+  _handleFocusOut(text) {
+    console.log(text);
+    let newMessage = {
+      userId: +sessionStorage.getItem("userId"),
+      username: this.props.message.username,
+      message: text,
+      timestamp: this.props.message.timestamp,
+      id: this.props.message.id
+    };
+    this.props.editMessage(newMessage);
+  }
   render() {
     return (
       <div key={this.props.message.id} className="card">
         <div className="card-body">
           <div className="card-title">
             <h5>{this.props.message.username}</h5>
-            <EditableLabel text={this.props.message.message}
-                onFocusOut={this._handleFocusOut}>{this.props.message.message}</EditableLabel>
-            <a href="#"
+            <EditableLabel
+              text={this.props.message.message}
+              onKeyDown={this.keyPress}
+
+              onFocusOut={this._handleFocusOut}
+            >
+              {this.props.message.message}
+            </EditableLabel>
+            <a
+              href="#"
               onClick={() => this.props.deleteMessage(this.props.message.id)}
               className="card-link"
             >
