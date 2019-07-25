1e3ba26b470a6c5decea461f8d7564df73858f91
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-07-25 10:36:18 -0500

    created dash for task

---
+++ src/components/tasks/TaskDash.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+export default class TaskDash extends Component {
+
+    createTasks = () => {
+        let user = +sessionStorage.getItem("userId");
+        let taskArr = []
+        this.props.tasks.forEach( task => {
+            if (task.userId === user) {
+              taskArr.push(task)
+            }
+          })
+        return taskArr
+      }
+
+    render() {
+        return (
+            <React.Fragment>
+            <section className="tasks">
+            <h1>Task List</h1>
+                {
+
+                    this.createTasks(this.props.friends).map(task =>
+                        <div key ={task.id} >
+                            <div className="card-body">
+                                <div className="card-title">
+
+                                <h6>Task Name: {task.taskName}</h6>
+                                <p>Due Date: {task.completedate}</p>
+                                </div>
+                            </div>
+                        </div>
+                        )
+                }
+            </section>
+            </React.Fragment>
+        )
+    }
+}
+
+
+
+{/* <div key={this.props.task.id} className="card"></div>
+{
+this.createTasks(this.props.friends).map(task =>
+<div className="card-body">
+    <h5 className="card-title">{this.props.task.taskName}</h5>
+    <p>Due Date: {this.props.task.completedate}</p>
+
+    </div
+}
+</div>  */}
