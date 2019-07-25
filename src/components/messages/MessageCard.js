9de92938929f945784437336323bd364761ca565
Tyler Carpenter <tcarp55@gmail.com>
2019-07-25 09:53:25 -0500

    deleted a few unused functions

--- src/components/messages/MessageCard.js
+++ src/components/messages/MessageCard.js
@@ -9,16 +9,6 @@ export default class MessageCard extends Component {
     this._handleFocusOut = this._handleFocusOut.bind(this);
   }
 
-  _handleFocus(text) {
-    console.log("Focused with text: " + text);
-  }
-
-  keyPress(e){
-    if(e.keyCode == 13){
-       console.log('value', e.target.text);
-       // put the login here
-    }
- };
 
   _handleFocusOut(text) {
     console.log(text);
@@ -36,11 +26,9 @@ export default class MessageCard extends Component {
       <div key={this.props.message.id} className="card">
         <div className="card-body">
           <div className="card-title">
-            <h5>{this.props.message.username}</h5>
+            <h5 onClick={() => {console.log("click")}} >{this.props.message.username}</h5>
             <EditableLabel
               text={this.props.message.message}
-              onKeyDown={this.keyPress}
-
               onFocusOut={this._handleFocusOut}
             >
               {this.props.message.message}
